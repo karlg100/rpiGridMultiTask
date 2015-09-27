b5657bd4e77156cbf0f0b0c713cb1860a973029b
Karl G <karl@spacechimp.net>
2015-09-27 19:28:00 +0000

    added test case script for omxplayer wrapper

---
+++ omxplayer.py
@@ -0,0 +1,35 @@
+from pyomxplayer import OMXPlayer
+from time import sleep
+from pprint import pprint
+
+# This will start an `omxplayer` process, this might 
+# fail the first time you run it, currently in the 
+# process of fixing this though.
+player = OMXPlayer('/home/pi/test.mp4')
+
+# The player will initially be paused
+
+player.toggle_pause()
+pprint(player.__dict__)
+#sleep(5)
+#player.toggle_pause()
+#pprint(player.__dict__)
+#sleep(5)
+#player.toggle_pause()
+#pprint(player.__dict__)
+#sleep(5)
+player.toggle_mute()
+pprint(player.__dict__)
+sleep(5)
+player.toggle_mute()
+pprint(player.__dict__)
+sleep(5)
+player.toggle_mute(.1)
+pprint(player.__dict__)
+sleep(5)
+player.toggle_mute(.5)
+pprint(player.__dict__)
+sleep(30)
+
+# Kill the `omxplayer` process gracefully.
+player.stop()
