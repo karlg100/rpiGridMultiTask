752132ddf4d54782ee10f6e853910852b1af4a22
Karl G <karl@spacechimp.net>
2015-09-29 09:11:51 +0000

    updaates

--- omxplayer.py
+++ omxplayer.py
@@ -8,20 +8,66 @@ import random
 import sys
 import time
 
+if len(sys.argv) > 1:
+    playerID =  int(sys.argv[1])
+else:
+    raise RuntimeError("specify player id: omxplayer.py [id]")
+
 player = False
+playerstatus = "standby"
 exitnow = False
 def playerHandler():
+    global playerID
+    context = zmq.Context()
+
+    work_receiver = context.socket(zmq.SUB)
+    port = 5501
+    work_receiver.Linger = 250
+    work_receiver.connect("tcp://192.168.1.233:5501")
+
+    #poller = zmq.Poller()
+    #poller.register(work_receiver, zmq.POLLIN)
+    work_receiver.setsockopt(zmq.SUBSCRIBE, "1")
+    work_receiver.setsockopt(zmq.SUBSCRIBE, "2")
+    #work_receiver.setsockopt(zmq.SUBSCRIBE, str(playerID))
+
     while not exitnow:
         print "listen here"
+        string = work_receiver.recv()
+        print string
+        topic, messagedata = string.split(" ", 1)
+        doCommand(messagedata)
+        #socks = dict(poller.poll(1000))
+        #if socks:
+                #if socks.get(work_receiver) == zmq.POLLIN:
         sleep(1)
 
-def playFile(file, args):
-    global player
-    player = OMXPlayer(file, args)
-    # The player will initially be paused
-    player.toggle_mute()
-    player.toggle_pause()
 
+def doCommand(cmdstr):
+    try:
+        cmd, messagedata = cmdstr.split(" ", 1)
+    except:
+        cmd = cmdstr
+    if cmd == "reset":
+        playerReset()
+    elif cmd == "queue":
+        args = json.loads(messagedata)
+        playFile(args)
+
+def playerReset():
+    global playerstatus
+    if player:
+        player.stop()
+    playerstatus = "reset"
+
+def playFile(args):
+    global player
+    player = OMXPlayer(args["file"], args["args"])
+    # The player will initially be paused
+    if args["mute"]:
+        player.toggle_mute()
+
+#    player.toggle_pause()
 
 #player.toggle_mute()
 #player.toggle_mute(.1)
@@ -30,12 +76,9 @@ def playFile(file, args):
 port = "5556"
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
+socket.linger = 250
 socket.bind("tcp://*:%s" % port)
 
-if len(sys.argv) > 1:
-    playerID =  int(sys.argv[1])
-else:
-    raise RuntimeError("specify player id: omxplayer.py [id]")
 
 
 def sendUpdate(messagedata):
@@ -49,7 +92,7 @@ t.start()
 try:
     while True:
         if not player:
-            sendUpdate(json.dumps({"player_status": "ready"}))
+            sendUpdate(json.dumps({"player_status": playerstatus}))
         else:
             messagedata = json.dumps({
 		"audio": player.__dict__['audio'],
@@ -60,16 +103,12 @@ try:
 		"paused": player.__dict__['paused'],
 		"position": player.__dict__['position'],
 		"title": player.__dict__['title'],
+                "player_running": player.is_running(),
+                "player_status": "running",
 		})
             sendUpdate(messagedata)
         sleep(1)
 except KeyboardInterrupt:
     exitnow = True
-    if player:
-        player.stop()
+    playerReset()
 
-#socket_sub.setsockopt(zmq.SUBSCRIBE, 1)
-#
-#poller = zmq.Poller()
-#    poller.register(socket_pull, zmq.POLLIN)
-#    poller.register(socket_sub, zmq.POLLIN)
