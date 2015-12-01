92c6cb91c42d326a757b49cf8e9aa2cd23eb79e9
Karl G <karl@spacechimp.net>
2015-12-01 19:28:39 +0000

    updated led count

--- neoPixels/playAll.py
+++ neoPixels/playAll.py
@@ -29,7 +29,7 @@ print "Playing sequences in random from the following moduels"
 pprint(modules)
 
 # LED strip configuration:
-LED_COUNT      = 480     # Number of LED pixels.
+LED_COUNT      = 632     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
